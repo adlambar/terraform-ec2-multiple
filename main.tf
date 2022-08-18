8d1b56b2f026d2cffab147624a91db3b2920ca55
adlambar <48596797+adlambar@users.noreply.github.com>
2022-08-18 16:46:33 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,4 +19,12 @@ resource "aws_instance" "instance" {
   tags = {    
     Name = "${var.instance_name}-${count.index}"
   }
+  ebs_block_device {
+    device_name = "/dev/xvdba"
+    tags = {
+       FileSystem = "/web/data"
+    }
+    volume_size = 2
+    volume_type = gp3
+  }
 }
