bb928d2b88a2c8aa905926b2e6bac4262d813577
adlambar <48596797+adlambar@users.noreply.github.com>
2022-08-19 13:24:28 -0300

    Update init-script_ubuntu.sh

--- init-script_ubuntu.sh
+++ init-script_ubuntu.sh
@@ -1,6 +1,10 @@
 #!/bin/bash 
 $(date > /tmp/date.txt)
+#To create partition
 echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdf
 mkfs -t ext4 /dev/xvdf1
 mkdir /storage1
 mount /dev/xvdf1 /storage1/
+
+#To grow partition
+growpart /dev/xvdf 1
