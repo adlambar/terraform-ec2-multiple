cb2de806345be37dc560b4bb907b1dd3a5984521
adlambar <48596797+adlambar@users.noreply.github.com>
2022-08-22 08:55:53 -0300

    Create init-script_rh.sh

---
+++ init-script_rh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+$(date > /tmp/date.txt)
+#To create partition
+
+echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdf
+mkfs -t xfs /dev/xvdf1
+mkdir -p /hana/shared/
+mount /dev/xvdf1 /hana/shared/
+
+echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdg
+mkfs -t xfs /dev/xvdg1
+mkdir -p /hana/data
+mount /dev/xvdg1 /hana/data
+
+echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdh
+mkfs -t xfs /dev/xvdh1
+mkdir -p /hana/log
+mount /dev/xvdh1 /hana/log
