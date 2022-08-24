55111510603cc87233b63a780bb08470d360cac0
adlambar <48596797+adlambar@users.noreply.github.com>
2022-08-24 08:48:01 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -72,6 +72,7 @@ variable "volumes" {
 Free Tiers for N. Virginia
 Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type     ami-090fa75af13c156b4
 Red Hat Enterprise Linux 8 (HVM), SSD Volume Type           ami-06640050dc3f556bb
+Red Hat Enterprise Linux 8 + Jenkins + Packer 		    ami-042b275a369428cc7	
 SUSE Linux Enterprise Server 15 SP4 (HVM), SSD Volume Type  ami-08e167817c87ed7fd
 Ubuntu Server 22.04 LTS (HVM), SSD Volume Type              ami-052efd3df9dad4825
 Debian 11 (HVM), SSD Volume Type                            ami-09a41e26df464c548
