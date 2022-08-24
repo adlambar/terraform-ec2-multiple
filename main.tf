78fda73a9fed150cee232c6d184342a3bbfa2254
adlambar <48596797+adlambar@users.noreply.github.com>
2022-08-24 09:57:14 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,32 +10,31 @@ data "aws_security_groups" "sgs" {
 }
 
 resource "aws_instance" "instance" {  
-  count                     = var.instances_count
-  ami                       = var.instance_ami
+  count                     = length(var.instance_config)
+  ami                       = var.instance_config[count.index].ami
   instance_type             = var.instance_type
   key_name                  = "ec2_keypair"
   vpc_security_group_ids    = data.aws_security_groups.sgs.ids
-  user_data                 = file(var.instance_user_data)
+  user_data                 = file(var.instance_config[count.index].userdata)
   tags = {    
-    Name = "${var.instance_name}-${count.index}"
+    Name = "${var.instance_config[count.index].Name}"
   }  
 }
 
 resource "aws_ebs_volume" "ebs_volume" {
-  count             = var.instances_count * length(var.volumes)
+  count             = length(var.instance_config) * length(var.volumes)
   availability_zone = aws_instance.instance[floor(count.index / length(var.volumes))].availability_zone
   size              = var.volumes[count.index % length(var.volumes)].size
   type              = var.volumes[count.index % length(var.volumes)].type
   tags = {
-    Name = "${var.instance_name}-${floor(count.index / length(var.volumes))}-ebs-volume-${var.volumes[count.index % length(var.volumes)].Name}"   
+    Name = "${var.instance_config[floor(count.index / length(var.volumes))].ami}-ebs-volume-${var.volumes[count.index % length(var.volumes)].Name}"   
   }
 }
 
 resource "aws_volume_attachment" "ebs_att" {
-  count       = var.instances_count * length(var.volumes)
+  count       = length(var.instance_config) * length(var.volumes)
   stop_instance_before_detaching = true
   device_name = "${var.instance_device_names[count.index % length(var.volumes)]}"
   volume_id   = aws_ebs_volume.ebs_volume[count.index].id
-  instance_id = aws_instance.instance[floor(count.index / length(var.volumes))].id 
-  #force_detach = true
+  instance_id = aws_instance.instance[floor(count.index / length(var.volumes))].id  
 }
