0f48e243364b2ce895ceeeeed114180682856c08
adlambar <48596797+adlambar@users.noreply.github.com>
2022-08-25 12:00:59 -0300

    Update init-script_rh.sh

--- init-script_rh.sh
+++ init-script_rh.sh
@@ -1,18 +1,22 @@
 #!/bin/bash 
 $(date > /tmp/date.txt)
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+
 #To create partition
+#echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdf
+#mkfs -t xfs /dev/xvdf1
+#mkdir -p /hana/shared/
+#mount /dev/xvdf1 /hana/shared/
 
-echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdf
-mkfs -t xfs /dev/xvdf1
-mkdir -p /hana/shared/
-mount /dev/xvdf1 /hana/shared/
+#echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdg
+#mkfs -t xfs /dev/xvdg1
+#mkdir -p /hana/data
+#mount /dev/xvdg1 /hana/data
 
-echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdg
-mkfs -t xfs /dev/xvdg1
-mkdir -p /hana/data
-mount /dev/xvdg1 /hana/data
-
-echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdh
-mkfs -t xfs /dev/xvdh1
-mkdir -p /hana/log
-mount /dev/xvdh1 /hana/log
+#echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdh
+#mkfs -t xfs /dev/xvdh1
+#mkdir -p /hana/log
+#mount /dev/xvdh1 /hana/log
